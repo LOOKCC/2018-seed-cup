221b3a1c4df18c4b2d14f15e790f9f96910a074e
wangzhihao <1397665447@qq.com>
2018-10-25 17:05:09 +0800

    add save_xgb

--- data_mining/xgb_save.py
+++ data_mining/xgb_save.py
@@ -12,6 +12,7 @@ from sklearn.feature_extraction.text import TfidfTransformer
 import pickle
 from sklearn.metrics import f1_score
 from scipy.sparse import hstack
+import os
 
 
 def get_train_words(train_data):
@@ -59,7 +60,7 @@ def get_label_data(test_data, keys, args):
         return [x for x in test_data if (x[cate1] == keys[0] and x[cate2] == keys[1])]
 
 
-def train(args):
+def train(args, num_round_1, num_round_2, num_round_3):
     f = open(args.class_info, 'rb')
     class_info = pickle.load(f)
     train_leveled_data, _ = load_leveled_data(args.train_file)
@@ -78,10 +79,14 @@ def train(args):
     # test_data_1 = load_data(args.test_file)
     param = {'max_depth': 8, 'eta': 0.5, 'eval_metric': 'merror', 'silent': 1,
              'objective': 'multi:softmax', 'num_class': len(label2idx)}  # 参数
-    num_round = 150  # 循环次数
     dtrain = xgb.DMatrix(title, label=train_label)
     evallist = [(dtrain, 'train')]
-    bst = xgb.train(param, dtrain, num_round, evallist)
+    if os.path.exists('./models/cate1.pkl'):
+        local_data = pickle.load(open('./models/cate1.pkl', 'rb'))
+        bst = local_data[0]
+        bst.train(param, dtrain, num_round_1, evallist)
+    else:
+        bst = xgb.train(param, dtrain, num_round_1, evallist)
     # save xgb train_words vectorizer
     to_save = [bst, vectorizer_1, train_words]
     with open('./models/cate1.pkl', 'wb') as f:
@@ -102,10 +107,14 @@ def train(args):
         # test_data_2 = get_label_data(test_data_1, [key_1], args)
         param = {'max_depth': 7, 'eta': 0.5, 'eval_metric': 'merror', 'silent': 1,
                  'objective': 'multi:softmax', 'num_class': len(label2idx)}  # 参数
-        num_round = 150  # 循环次数
         dtrain = xgb.DMatrix(title, label=train_label)
         evallist = [(dtrain, 'train')]
-        bst = xgb.train(param, dtrain, num_round, evallist)
+        if os.path.exists('./models/cate2_'+str(key_1)+'.pkl'):
+            local_data = pickle.load(open('./models/cate2_'+str(key_1)+'.pkl', 'rb'))
+            bst = local_data[0]
+            bst.train(param, dtrain, num_round_2, evallist)
+        else:
+            bst = xgb.train(param, dtrain, num_round_2, evallist)
         to_save = [bst, vectorizer_2, train_words]
         with open('./models/cate2_'+str(key_1)+'.pkl', 'wb') as f:
             pickle.dump(to_save, f)
@@ -125,10 +134,14 @@ def train(args):
             # test_data_2 = get_label_data(test_data_1, [key_1], args)
             param = {'max_depth': 7, 'eta': 0.5, 'eval_metric': 'merror', 'silent': 1,
                      'objective': 'multi:softmax', 'num_class': len(label2idx)}  # 参数
-            num_round = 200  # 循环次数
             dtrain = xgb.DMatrix(title, label=train_label)
             evallist = [(dtrain, 'train')]
-            bst = xgb.train(param, dtrain, num_round, evallist)
+            if os.path.exists('./models/cate3_'+str(key_1)+'_'+str(key_2)+'.pkl'):
+                local_data = pickle.load(open('./models/cate3_'+str(key_1)+'_'+str(key_2)+'.pkl', 'rb'))
+                bst = local_data[0]
+                bst.train(param, dtrain, num_round_3, evallist)
+            else:
+                bst = xgb.train(param, dtrain, num_round_3, evallist)
             to_save = [bst, vectorizer_3, train_words]
             with open('./models/cate3_'+str(key_1)+'_'+str(key_2)+'.pkl', 'wb') as f:
                 pickle.dump(to_save, f)
@@ -211,44 +224,48 @@ def val(val_result):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_file', type=str, help='Training Data file')
-    parser.add_argument('--test_file', type=str, help='Testing data file')
-    parser.add_argument('--class_info', type=str,
+    parser.add_argument('--train_file', type=str,default='../data/train_b.txt', help='Training Data file')
+    parser.add_argument('--test_file', type=str, default='../data/valid_b.txt',help='Testing data file')
+    parser.add_argument('--class_info', type=str, default='../data/class_info.txt',
                         help='word to idx file, which has deleted the uncommonly uesd words')
     parser.add_argument('--test', action='store_true', help='train or test')
+    parser.add_argument('--epoch', type=int, default=100,help='epoch to train')
 
     args = parser.parse_args()
 
-    test_file = args.test_file
-    args.test_file = args.train_file
-    # train(args)
-    train_result = valid(args)
-    args.test_file = test_file
-    test_result = valid(args)
-    if not args.test:
+    for i in range(args.epoch//10):
+        test_file = args.test_file
+        args.test_file = args.train_file
+        train(args, 10, 10 ,10)
+        train_result = valid(args)
+        args.test_file = test_file
+        test_result = valid(args)
         print('-----train f1-----')
         val(train_result)
         print('-----test f1-----')
         val(test_result)
-    else:
-        with open("../output/submit.txt", "w") as f:
-            f.write("item_id\tcate1_id\tcate2_id\tcate3_id\n")
-            for x in result:
-                f.write(x[0]+'\t'+str(x[5])+'\t'+str(x[6])+'\t'+str(x[7])+'\n')
-                # f.write(x[0]+'\t'+str(x[5])+'\t'+str(x[6])+'\t'+str(x[7])+'\t'+str(x[8])+'\t'+str(x[9])+'\t'+str(x[10])+'\n')
-        # make the order the same with test_file
-        finall = []
-        with open("../output/submit.txt", 'r') as f:
-            f.readline()
-            lines_sub = f.readlines()
-        with open(args.test_file, 'r') as f:
-            f.readline()
-            lines_test = f.readlines()
-        for test_line in lines_test:
-            for sub_line in lines_sub:
-                if test_line[0:33] == sub_line[0:33]:
-                    finall.append(sub_line)
-        with open("../output/submit_ordered.txt", 'w') as f:
-            f.write("item_id\tcate1_id\tcate2_id\tcate3_id\n")
-            for line in finall:
-                f.write(line)
+
+    # if not args.test:
+
+    # else:
+    #     with open("../output/submit.txt", "w") as f:
+    #         f.write("item_id\tcate1_id\tcate2_id\tcate3_id\n")
+    #         for x in result:
+    #             f.write(x[0]+'\t'+str(x[5])+'\t'+str(x[6])+'\t'+str(x[7])+'\n')
+    #             # f.write(x[0]+'\t'+str(x[5])+'\t'+str(x[6])+'\t'+str(x[7])+'\t'+str(x[8])+'\t'+str(x[9])+'\t'+str(x[10])+'\n')
+    #     # make the order the same with test_file
+    #     finall = []
+    #     with open("../output/submit.txt", 'r') as f:
+    #         f.readline()
+    #         lines_sub = f.readlines()
+    #     with open(args.test_file, 'r') as f:
+    #         f.readline()
+    #         lines_test = f.readlines()
+    #     for test_line in lines_test:
+    #         for sub_line in lines_sub:
+    #             if test_line[0:33] == sub_line[0:33]:
+    #                 finall.append(sub_line)
+    #     with open("../output/submit_ordered.txt", 'w') as f:
+    #         f.write("item_id\tcate1_id\tcate2_id\tcate3_id\n")
+    #         for line in finall:
+    #             f.write(line)
