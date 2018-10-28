bdb4303404f6d77e5531b77f5efdb1ee1a962333
1@1.com <1@1.com>
2018-10-28 12:18:54 +0800

    nothing

--- output/count_cate.py
+++ output/count_cate.py
@@ -1,18 +1,21 @@
 import numpy as np
+
+
 def count(label):
     if label in cate_count:
         cate_count[label] += 1
     else:
         cate_count[label] = 1
 
+
 if __name__ == '__main__':
     cate_count = {}
-    with open('subumit_ordered.txt', 'r') as f:
+    with open('submit_ordered.txt', 'r') as f:
         f.readline()
         lines = f.readlines()
         for line in lines:
-            line = line.split('\t')
-            count(line[1])    
-            count(line[2])    
-            count(line[3]) 
-    print(cate_count)   
+            line = line.strip().split('\t')
+            count(line[1])
+            count(line[2])
+            count(line[3])
+    print(cate_count)
