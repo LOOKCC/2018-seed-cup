fac8a15aca577267ac501a4a7a28f5b80511203d
platoneko <platonekosama@gmail.com>
2018-11-02 21:29:20 +0800

    change glu

---
+++ glu/eval.py
@@ -0,0 +1,114 @@
+import torch
+import torch.utils.data as data
+import torch.backends.cudnn as cudnn
+import pickle
+import os
+
+import argparse
+
+from sklearn.metrics import f1_score
+
+from models.cate1_classifier import Cate1Classifier
+from models.cate2_classifier import Cate2Classifier
+from models.cate3_classifier import Cate3Classifier
+from utils.dataset import TrainDataset, padding
+
+
+WORDS_CNT = 72548
+CHARS_CNT = 4933
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+valid_words_path = './preproc/valid_words.pkl'
+valid_chars_path = './preproc/valid_chars.pkl'
+valid_cate_path = './preproc/valid_cate.pkl'
+
+def eval(clf1, clf2, clf3, dataloader):
+    clf1.eval()
+    clf2.eval()
+    clf3.eval()
+    pred1, pred2, pred3 = [], [], []
+    target1, target2, target3 = [], [], []
+    for title, desc, cate1, cate2, cate3, t_len, d_len in dataloader:
+        title = title.to(device)
+        output1 = clf1(title, desc, t_len, d_len)
+        output1 = output1.argmax(1)
+        pred1.extend(output1.tolist())
+        output2 = clf2(title, desc, t_len, d_len, output1)
+        output2 = output2.argmax(1)
+        pred2.extend(output2.tolist())
+        output3 = clf3(title, desc, t_len, d_len, output2)
+        pred3.extend(output3.argmax(1).tolist())
+        target1.extend(cate1.tolist())
+        target2.extend(cate2.tolist())
+        target3.extend(cate3.tolist())
+    cate1_score = f1_score(target1, pred1, average='macro')
+    cate2_score = f1_score(target2, pred2, average='macro')
+    cate3_score = f1_score(target3, pred3, average='macro')
+    score = 0.1 * cate1_score + 0.3 * cate2_score + 0.6 * cate3_score
+    print('Validation cate1 f1 score: {:.4}'.format(cate1_score))
+    print('Validation cate2 f1 score: {:.4}'.format(cate2_score))
+    print('Validation cate3 f1 score: {:.4}'.format(cate3_score))
+    print('Validation weighted f1 score: {:.4}'.format(score))
+
+def parse_cmd():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', type=int, default=64,
+                        help='batch size, default=64')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_cmd()
+
+    with open(valid_words_path, 'rb') as fp:
+        features = pickle.load(fp)
+    title = [feature[0] for feature in features]
+    desc = [feature[1] for feature in features]
+    t_len = [len(feature[0]) for feature in features]
+    d_len = [len(feature[1]) for feature in features]
+
+    with open(valid_cate_path, 'rb') as fp:
+        cate = pickle.load(fp)
+    cate1 = [ca[0] for ca in cate]
+    cate2 = [ca[1] for ca in cate]
+    cate3 = [ca[2] for ca in cate]
+
+    title = padding(title, max(t_len))
+    desc = padding(desc, max(d_len))
+    t_len = torch.tensor(t_len)
+    d_len = torch.tensor(d_len)
+
+    cate1 = torch.tensor(cate1)
+    cate2 = torch.tensor(cate2)
+    cate3 = torch.tensor(cate3)
+
+    valid_set = TrainDataset(title, desc, cate1, cate2, cate3, t_len, d_len)
+    valid_loader = data.DataLoader(valid_set, batch_size=args.batch_size, num_workers=4)
+
+    clf1_state = torch.load(os.path.join('checkpoint', 'glu_w_cate1.pth'))
+    clf1 = Cate1Classifier(WORDS_CNT+1, clf1_state['args'])
+    clf1.load_state_dict(clf1_state['model'])
+
+    with open('./preproc/mask.pkl', 'rb') as fp:
+        mask1, mask2 = pickle.load(fp)
+
+    clf2_state = torch.load(os.path.join('checkpoint', 'glu_w_cate2.pth'))
+    clf2 = Cate2Classifier(WORDS_CNT+1, clf2_state['args'], mask1=mask1)
+    clf2.load_state_dict(clf2_state['model'])
+
+    clf3_state = torch.load(os.path.join('checkpoint', 'glu_w_cate3.pth'))
+    clf3 = Cate3Classifier(WORDS_CNT+1, clf3_state['args'], mask2=mask2)
+    clf3.load_state_dict(clf3_state['model'])
+
+    clf1 = clf1.to(device)
+    clf2 = clf2.to(device)
+    clf3 = clf3.to(device)
+
+    if torch.cuda.is_available():
+        cudnn.benchmark = True
+
+    with torch.no_grad():
+        eval(clf1, clf2, clf3, valid_loader)
+
---
+++ glu/train.py
@@ -0,0 +1,182 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torch.backends.cudnn as cudnn
+import pickle
+import argparse
+import os
+
+from utils.dataset import TrainDataset, padding
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_word_path = '../swem/preproc/train_words.pkl'
+valid_word_path = '../swem/preproc/valid_words.pkl'
+train_cate_path = '../swem/preproc/train_cate.pkl'
+valid_cate_path = '../swem/preproc/valid_cate.pkl'
+
+WORDS_CNT = 72548
+CHARS_CNT = 4933
+
+def parse_cmd():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model', type=int, default=1,
+                        help='train cate m classifier, default=1')
+    parser.add_argument('-r', '--resume', action='store_true',
+                        help='resume from checkpoint')
+    parser.add_argument('--batch_size', type=int, default=64,
+                        help='batch size, default=64')
+    parser.add_argument('--lr', type=float, default=1e-3,
+                        help='learning rate, default=1e-3')
+    parser.add_argument('--weight_decay', type=float, default=0,
+                        help='l2 weight decay, default=0')
+    parser.add_argument('--h_d', type=int, default=128,
+                        help='hidden dim, default=128')
+    parser.add_argument('--ckpt', required=True,
+                        help='load/save checkpoint path')
+    parser.add_argument('--embedding_dim', type=int, default=512,
+                        help='word/char embedding dim, default=512')
+    parser.add_argument('--class_weight', type=int, default=0,
+                        help='add class weight on loss, default=0')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_cmd()
+
+    best_score = 0
+    start_epoch = 0
+
+    ckpt = os.path.join('checkpoint', args.ckpt)
+
+    input_size = WORDS_CNT + 1
+    train_feature_path = train_word_path
+    valid_feature_path = valid_word_path
+
+    if args.model == 1:
+        from models.cate1_classifier import *
+        if args.resume:
+            state = torch.load(ckpt)
+            model = Cate1Classifier(input_size, state['args'])
+            model.load_state_dict(state['model'])
+            start_epoch = state['epoch'] + 1
+            best_score = state['best_score']
+        else:
+            word2vec = torch.load('../swem/preproc/word2vec.pth')
+            model = Cate1Classifier(input_size, args, word2vec)
+
+    elif args.model == 2:
+        from models.cate2_classifier import *
+        if args.resume:
+            state = torch.load(ckpt)
+            with open('../swem/preproc/mask.pkl', 'rb') as fp:
+                mask1, mask2 = pickle.load(fp)
+            model = Cate2Classifier(input_size, state['args'], mask1=mask1)
+            model.load_state_dict(state['model'])
+            start_epoch = state['epoch'] + 1
+            best_score = state['best_score']
+        else:
+            word2vec = torch.load('../swem/preproc/word2vec.pth')
+            with open('../swem/preproc/mask.pkl', 'rb') as fp:
+                mask1, mask2 = pickle.load(fp)
+            model = Cate2Classifier(input_size, args, word2vec, mask1)
+
+    elif args.model == 3:
+        from models.cate3_classifier import *
+        if args.resume:
+            state = torch.load(ckpt)
+            with open('../swem/preproc/mask.pkl', 'rb') as fp:
+                mask1, mask2 = pickle.load(fp)
+            model = Cate3Classifier(input_size, state['args'], mask2=mask2)
+            model.load_state_dict(state['model'])
+            start_epoch = state['epoch'] + 1
+            best_score = state['best_score']
+        else:
+            word2vec = torch.load('../swem/preproc/word2vec.pth')
+            with open('../swem/preproc/mask.pkl', 'rb') as fp:
+                mask1, mask2 = pickle.load(fp)
+            model = Cate3Classifier(input_size, args, word2vec, mask2)
+
+    else:
+        raise Exception
+
+    model = model.to(device)
+    if torch.cuda.is_available():
+        cudnn.benchmark = True
+
+    if args.class_weight:
+        with open('../swem/utils/class_weight.pkl', 'rb') as fp:
+            class_weight = pickle.load(fp)
+        criterion = nn.CrossEntropyLoss(weight=class_weight[args.model-1].to(device))
+    else:
+        criterion = nn.CrossEntropyLoss()
+
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    if args.resume:
+        optimizer.load_state_dict(state['optimizer'])
+
+    with open(train_feature_path, 'rb') as fp:
+        train_features = pickle.load(fp)
+
+    train_title = [feature[0] for feature in train_features]
+    train_desc = [feature[1] for feature in train_features]
+    train_t_len = [len(feature[0]) for feature in train_features]
+    train_d_len = [len(feature[1]) for feature in train_features]
+
+    with open(train_cate_path, 'rb') as fp:
+        train_cate = pickle.load(fp)
+    train_cate1 = [cate[0] for cate in train_cate]
+    train_cate2 = [cate[1] for cate in train_cate]
+    train_cate3 = [cate[2] for cate in train_cate]
+
+    train_title = padding(train_title, max(train_t_len))
+    train_desc = padding(train_desc, max(train_d_len))
+
+    train_t_len = torch.tensor(train_t_len)
+    train_d_len = torch.tensor(train_d_len)
+
+    train_cate1 = torch.tensor(train_cate1)
+    train_cate2 = torch.tensor(train_cate2)
+    train_cate3 = torch.tensor(train_cate3)
+
+    train_set = TrainDataset(train_title, train_desc,
+                          train_cate1, train_cate2, train_cate3,
+                          train_t_len, train_d_len)
+    train_loader = data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=4)
+
+    with open(valid_feature_path, 'rb') as fp:
+        valid_features = pickle.load(fp)
+    valid_title = [feature[0] for feature in valid_features]
+    valid_desc = [feature[1] for feature in valid_features]
+    valid_t_len = [len(feature[0]) for feature in valid_features]
+    valid_d_len = [len(feature[1]) for feature in valid_features]
+
+    with open(valid_cate_path, 'rb') as fp:
+        valid_cate = pickle.load(fp)
+    valid_cate1 = [cate[0] for cate in valid_cate]
+    valid_cate2 = [cate[1] for cate in valid_cate]
+    valid_cate3 = [cate[2] for cate in valid_cate]
+
+    valid_title = padding(valid_title, max(valid_t_len))
+    valid_desc = padding(valid_desc, max(valid_d_len))
+    valid_t_len = torch.tensor(valid_t_len)
+    valid_d_len = torch.tensor(valid_d_len)
+
+    valid_cate1 = torch.tensor(valid_cate1)
+    valid_cate2 = torch.tensor(valid_cate2)
+    valid_cate3 = torch.tensor(valid_cate3)
+
+    valid_set = TrainDataset(valid_title, valid_desc,
+                          valid_cate1, valid_cate2, valid_cate3,
+                          valid_t_len, valid_d_len)
+    valid_loader = data.DataLoader(valid_set, batch_size=args.batch_size, num_workers=4)
+
+    for epoch in range(start_epoch, start_epoch+20):
+        train_epoch(epoch, model, train_loader, criterion, optimizer, args)
+        with torch.no_grad():
+            best_score = eval_epoch(epoch, model, valid_loader, best_score, optimizer, args)
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+
